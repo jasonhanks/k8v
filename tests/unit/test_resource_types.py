43130f766bb7bd0070de8b5d0137f65b1c943462
Jason hanks <jason.e.hanks@gmail.com>
2022-01-26 22:11:52 -0600

    Use a dict() to specify aliases

--- tests/unit/test_resource_types.py
+++ tests/unit/test_resource_types.py
@@ -13,7 +13,7 @@ class TestResourceTypes:
                 assert k8v.resource_types.ResourceType.from_value(alias) == type
 
     def test_invalid_from_value(self):
-        """Validate that we cannot use an invalid alias for a known type."""
+        """Validate that we cannot use an invalid alias."""
         assert k8v.resource_types.ResourceType.from_value(TestResourceTypes) == None
         assert k8v.resource_types.ResourceType.from_value(None) == None
         assert k8v.resource_types.ResourceType.from_value(True) == None
@@ -53,99 +53,100 @@ class TestResourceTypes:
     def test_type_aliases(self):
         """Validate that aliases for the supported resource types are supported."""
 
-        assert [x for x in k8v.resource_types.ResourceType.CLUSTER_ROLES.value] == [
-            "clusterrole",
-            "clusterroles",
-        ]
-        assert [
-            x for x in k8v.resource_types.ResourceType.CLUSTER_ROLE_BINDINGS.value
-        ] == ["clusterrolebinding", "clusterrolebindings"]
-        assert [x for x in k8v.resource_types.ResourceType.ROLES.value] == [
-            "role",
-            "roles",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.ROLE_BINDINGS.value] == [
-            "rolebinding",
-            "rolebindings",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.CONFIG_MAP.value] == [
-            "configmap",
-            "configmaps",
-            "cm",
-            "cms",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.CRONJOBS.value] == [
-            "cronjob",
-            "cronjobs",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.DAEMON_SETS.value] == [
-            "daemonset",
-            "daemonsets",
-            "ds",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.DEPLOYMENTS.value] == [
-            "deployment",
-            "deployments",
-            "deploy",
-            "deploys",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.INGRESS.value] == [
-            "ingress",
-            "ingresses",
-            "ing",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.JOBS.value] == [
-            "job",
-            "jobs",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.NETWORK_POLICY.value] == [
-            "networkpolicy",
-            "networkpolicies",
-            "netpol",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.PERSISTENT_VOLUME.value] == [
-            "persistentvolume",
-            "persistentvolumes",
-            "pv",
-        ]
-        assert [
-            x for x in k8v.resource_types.ResourceType.PERSISTENT_VOLUME_CLAIM.value
-        ] == ["persistentvolumeclaim", "persistentvolumeclaims", "pvc"]
-        assert [x for x in k8v.resource_types.ResourceType.PODS.value] == [
-            "pod",
-            "pods",
-            "po",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.REPLICA_SETS.value] == [
-            "replicaset",
-            "replicasets",
-            "rs",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.ROLES.value] == [
-            "role",
-            "roles",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.ROLE_BINDINGS.value] == [
-            "rolebinding",
-            "rolebindings",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.SECRETS.value] == [
-            "secret",
-            "secrets",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.SERVICES.value] == [
-            "service",
-            "services",
-            "svc",
-            "svcs",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.SERVICE_ACCOUNTS.value] == [
-            "serviceaccount",
-            "serviceaccounts",
-            "sa",
-        ]
-        assert [x for x in k8v.resource_types.ResourceType.STATEFUL_SETS.value] == [
-            "statefulset",
-            "statefulsets",
-            "sts",
-        ]
+        types = {
+            k8v.resource_types.ResourceType.CLUSTER_ROLES: [
+                "clusterrole",
+                "clusterroles",
+            ],
+            k8v.resource_types.ResourceType.CLUSTER_ROLE_BINDINGS: [
+                "clusterrolebinding",
+                "clusterrolebindings",
+            ],
+            k8v.resource_types.ResourceType.CONFIG_MAP: [
+                "configmap",
+                "configmaps",
+                "cm",
+                "cms",
+            ],
+            k8v.resource_types.ResourceType.CRONJOBS: [
+                "cronjob",
+                "cronjobs",
+            ],
+            k8v.resource_types.ResourceType.DAEMON_SETS: [
+                "daemonset",
+                "daemonsets",
+                "ds",
+            ],
+            k8v.resource_types.ResourceType.DEPLOYMENTS: [
+                "deployment",
+                "deployments",
+                "deploy",
+                "deploys",
+            ],
+            k8v.resource_types.ResourceType.INGRESS: [
+                "ingress",
+                "ingresses",
+                "ing",
+            ],
+            k8v.resource_types.ResourceType.JOBS: [
+                "job",
+                "jobs",
+            ],
+            k8v.resource_types.ResourceType.NETWORK_POLICY: [
+                "networkpolicy",
+                "networkpolicies",
+                "netpol",
+            ],
+            k8v.resource_types.ResourceType.PERSISTENT_VOLUME: [
+                "persistentvolume",
+                "persistentvolumes",
+                "pv",
+            ],
+            k8v.resource_types.ResourceType.PERSISTENT_VOLUME_CLAIM: [
+                "persistentvolumeclaim",
+                "persistentvolumeclaims",
+                "pvc",
+            ],
+            k8v.resource_types.ResourceType.PODS: [
+                "pod",
+                "pods",
+                "po",
+            ],
+            k8v.resource_types.ResourceType.REPLICA_SETS: [
+                "replicaset",
+                "replicasets",
+                "rs",
+            ],
+            k8v.resource_types.ResourceType.ROLES: [
+                "role",
+                "roles",
+            ],
+            k8v.resource_types.ResourceType.ROLE_BINDINGS: [
+                "rolebinding",
+                "rolebindings",
+            ],
+            k8v.resource_types.ResourceType.SECRETS: [
+                "secret",
+                "secrets",
+            ],
+            k8v.resource_types.ResourceType.SERVICES: [
+                "service",
+                "services",
+                "svc",
+                "svcs",
+            ],
+            k8v.resource_types.ResourceType.SERVICE_ACCOUNTS: [
+                "serviceaccount",
+                "serviceaccounts",
+                "sa",
+            ],
+            k8v.resource_types.ResourceType.STATEFUL_SETS: [
+                "statefulset",
+                "statefulsets",
+                "sts",
+            ],
+        }
+
+        for type, aliases in types.items():
+            for alias in aliases:
+                assert alias in type.value
